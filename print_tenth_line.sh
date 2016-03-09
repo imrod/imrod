293724106f6653f54de82cbb3418b55b5dedb37e
imrod <imrod.rod@gmail.com>
2016-03-10 00:00:14 +0800

    Create print_tenth_line.sh
    print tenth line script

---
+++ print_tenth_line.sh
@@ -0,0 +1,10 @@
+# Read from the file file.txt and output the tenth line to stdout.
+flag=1
+while read line
+do
+    if [ $flag -eq 10 ]; then
+        echo "$line"
+        exit 0
+    fi
+    ((flag++))
+done < file.txt
